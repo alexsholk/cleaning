0d56fe44d63a02e5a3baf10c2bd0e83dd1e6e4fa
Aleksandr Sholk <aleksandr.sholk@fortegrp.net>
2018-05-20 17:00:18 +0300

    Param fixtures

---
+++ src/DataFixtures/ParamFixtures.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\Generator\DataGenerator;
+use App\Entity\Order;
+use App\Entity\Param;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ParamFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $params = [];
+
+        /** Настройки сайта */
+
+        // Meta главной страницы
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Заголовок сайта')
+            ->setCode('SITE_TITLE')
+            ->setType(Param::TYPE_STRING)
+            ->setValue('Уборка квартир в Минске');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Описание сайта (SEO)')
+            ->setCode('SITE_META_DESCRIPTION')
+            ->setType(Param::TYPE_STRING)
+            ->setValue('Уборка квартир в Минске быстро и недорого.');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Ключевые слова (SEO)')
+            ->setCode('SITE_META_KEYWORDS')
+            ->setType(Param::TYPE_STRING)
+            ->setValue('Уборка квартир, клининговый сервис');
+
+        // Заголовок страницы заказа
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Заголовок страницы заказа')
+            ->setCode('SITE_ORDER_TITLE')
+            ->setType(Param::TYPE_STRING)
+            ->setValue('Оформление заказа');
+
+        // Телефон
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Телефон')
+            ->setCode('PHONE')
+            ->setType(Param::TYPE_PHONE)
+            ->setValue('+375 29 607 69 39');
+
+        // Социальные сети
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Ссылка на Facebook')
+            ->setCode('LINK_FACEBOOK')
+            ->setType(Param::TYPE_URL)
+            ->setValue('https://www.facebook.com/');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Ссылка на VK')
+            ->setCode('LINK_VK')
+            ->setType(Param::TYPE_URL)
+            ->setValue('https://vk.com/');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Ссылка на Instagram')
+            ->setCode('LINK_INSTAGRAM')
+            ->setType(Param::TYPE_URL)
+            ->setValue('https://www.instagram.com/');
+
+        // Слоган
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Слоган')
+            ->setCode('SITE_SLOGAN')
+            ->setType(Param::TYPE_STRING)
+            ->setValue('Мы работаем чтобы Вы отдыхали!');
+
+        // Прочие настройки
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_SITE_SETTINGS)
+            ->setTitle('Разрешить добавление отзывов через сайт')
+            ->setCode('REVIEW_ADD_ENABLED')
+            ->setType(Param::TYPE_BOOL)
+            ->setValue(true);
+
+        /** Текстовые блоки */
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_TEXT_BLOCKS)
+            ->setTitle('Блок ссылок на главной')
+            ->setCode('BLOCK_MAIN_LINKS')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(<<<HTML
+<ul>
+    <li><a href="/page/about">Поддерживающая уборка</a></li>
+    <li><a href="/page/about">Уборка после ремонта</a></li>
+    <li><a href="/page/about">Мытье окон</a></li>
+    <li><a href="/page/about">Мытье бытовой техники</a></li>
+    <li><a href="/page/about">Глажка белья</a></li>
+    <li><a href="/page/about" class="grey">Что мы не делаем</a></li>
+</ul>            
+HTML
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_TEXT_BLOCKS)
+            ->setTitle('Первый блок текста на главной')
+            ->setCode('BLOCK_MAIN_TEXT_1')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Клининговая компания “Уборочка” в Минске</h3>' .
+                DataGenerator::generateText(10, true)
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_TEXT_BLOCKS)
+            ->setTitle('Второй блок текста на главной')
+            ->setCode('BLOCK_MAIN_TEXT_2')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Почему выбирают нас</h3>' .
+                DataGenerator::generateText(10, true)
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_TEXT_BLOCKS)
+            ->setTitle('Нижнее меню')
+            ->setCode('BLOCK_FOOTER_MENU')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(<<<HTML
+<ul>
+    <li><a href="/page/about">Вопросы и ответы</a></li>
+    <li><a href="/page/about">Хотите работать у нас?</a></li>
+    <li><a href="/page/about">Пользовательское соглашение</a></li>
+    <li><a href="/page/about">О сервисе</a></li>
+</ul>
+HTML
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_TEXT_BLOCKS)
+            ->setTitle('Скидки и акции')
+            ->setCode('BLOCK_OFFERS')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(DataGenerator::generateText(5, true));
+
+        /** Сообщения */
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об успешном добавлении отзыва')
+            ->setCode('REVIEW_ADD_SUCCESS_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Спасибо за отзыв</h3>' .
+                '<p>Ваш отзыв сохранен и будет опубликован после проверки администратором.</p>'
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об ошибке при добавлении отзыва')
+            ->setCode('REVIEW_ADD_ERROR_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Произошла ошибка</h3>' .
+                '<p>Произошла ошибка при сохранении отзыва. Скоро всё исправим.</p>'
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об успешном запросе звонка')
+            ->setCode('CALL_REQUEST_ADD_SUCCESS_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Запрос отправлен</h3>' .
+                '<p>Мы скоро вам перезвоним.</p>'
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об ошибке при запросе звонка')
+            ->setCode('CALL_REQUEST_ADD_ERROR_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Произошла ошибка</h3>' .
+                '<p>Произошла ошибка при отправке запроса. Скоро всё исправим.</p>'
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об успешном добавлении заказа')
+            ->setCode('ORDER_ADD_SUCCESS_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Спасибо за заказ</h3>' .
+                '<p>Ваш заказ принят. Ожидайте звонка менеджера.</p>'
+            );
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_MESSAGES)
+            ->setTitle('Сообщение об ошибке при добавлении заказа')
+            ->setCode('ORDER_ADD_ERROR_MESSAGE')
+            ->setType(Param::TYPE_HTML)
+            ->setValue(
+                '<h3>Произошла ошибка</h3>' .
+                '<p>Произошла ошибка при сохранении заказа. Скоро всё исправим.</p>'
+            );
+
+        /** Настройки заказов */
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Базовая стоимость заказа')
+            ->setCode('ORDER_BASE_COST')
+            ->setType(Param::TYPE_DOUBLE)
+            ->setValue(20.0);
+
+        // Скидка за периодичность
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Скидка за периодичность: раз в месяц')
+            ->setCode('FREQUENCY_DISCOUNT_'.Order::FREQUENCY_MONTHLY)
+            ->setType(Param::TYPE_INT)
+            ->setValue(10);
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Скидка за периодичность: раз в 2 недели')
+            ->setCode('FREQUENCY_DISCOUNT_'.Order::FREQUENCY_EVERY_TWO_WEEKS)
+            ->setType(Param::TYPE_INT)
+            ->setValue(15);
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Скидка за периодичность: раз в неделю')
+            ->setCode('FREQUENCY_DISCOUNT_'.Order::FREQUENCY_WEEKLY)
+            ->setType(Param::TYPE_INT)
+            ->setValue(20);
+
+        // Время заказа
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Минимальное время заказа')
+            ->setCode('ORDER_MIN_TIME')
+            ->setType(Param::TYPE_TIME)
+            ->setValue('00:00');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Максимальное время заказа')
+            ->setCode('ORDER_MAX_TIME')
+            ->setType(Param::TYPE_TIME)
+            ->setValue('23:59');
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Минимальное время до уборки (минут)')
+            ->setCode('ORDER_MIN_TIME_TO_CLEANING')
+            ->setType(Param::TYPE_INT)
+            ->setValue(240);
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Максимальное время до уборки (дней)')
+            ->setCode('ORDER_MAX_TIME_TO_CLEANING')
+            ->setType(Param::TYPE_INT)
+            ->setValue(30);
+
+        $params[] = (new Param())
+            ->setCategory(Param::CATEGORY_ORDER_SETTINGS)
+            ->setTitle('Интервал времени для списка в календаре')
+            ->setCode('ORDER_TIME_STEP')
+            ->setType(Param::TYPE_INT)
+            ->setValue(60);
+
+        // Сохранение параметров
+        foreach ($params as $param) {
+            $manager->persist($param);
+            $this->addReference($param->getCode(), $param);
+        }
+
+        $manager->flush();
+    }
+}
--- src/Entity/Param.php
+++ src/Entity/Param.php
@@ -12,6 +12,69 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Param
 {
+    // Категории параметров
+    const CATEGORY_TEXT_BLOCKS    = 0;
+    const CATEGORY_SITE_SETTINGS  = 1;
+    const CATEGORY_MESSAGES       = 2;
+    const CATEGORY_ORDER_SETTINGS = 3;
+
+    const CATEGORY_CODE_TEXT_BLOCKS    = 'group.text_blocks';
+    const CATEGORY_CODE_SITE_SETTINGS  = 'group.site_settings';
+    const CATEGORY_CODE_MESSAGES       = 'group.messages';
+    const CATEGORY_CODE_ORDER_SETTINGS = 'group.order_settings';
+
+    public static $groups = [
+        self::CATEGORY_TEXT_BLOCKS    => self::CATEGORY_CODE_TEXT_BLOCKS,
+        self::CATEGORY_SITE_SETTINGS  => self::CATEGORY_CODE_SITE_SETTINGS,
+        self::CATEGORY_MESSAGES       => self::CATEGORY_CODE_MESSAGES,
+        self::CATEGORY_ORDER_SETTINGS => self::CATEGORY_CODE_ORDER_SETTINGS,
+    ];
+
+    // Типы параметров
+    const TYPE_STRING    = 0;
+    const TYPE_EMAIL     = 1;
+    const TYPE_URL       = 2;
+    const TYPE_PHONE     = 3;
+    const TYPE_TEXT      = 4;
+    const TYPE_HTML      = 5;
+    const TYPE_INT       = 6;
+    const TYPE_DOUBLE    = 7;
+    const TYPE_BOOL      = 8;
+    const TYPE_DATE      = 9;
+    const TYPE_TIME      = 10;
+    const TYPE_DATETIME  = 11;
+    const TYPE_IMAGE_URL = 12;
+
+    const TYPE_CODE_STRING    = 'type.string';
+    const TYPE_CODE_EMAIL     = 'type.email';
+    const TYPE_CODE_URL       = 'type.url';
+    const TYPE_CODE_PHONE     = 'type.phone';
+    const TYPE_CODE_TEXT      = 'type.text';
+    const TYPE_CODE_HTML      = 'type.html';
+    const TYPE_CODE_INT       = 'type.int';
+    const TYPE_CODE_DOUBLE    = 'type.double';
+    const TYPE_CODE_BOOL      = 'type.bool';
+    const TYPE_CODE_DATE      = 'type.date';
+    const TYPE_CODE_TIME      = 'type.time';
+    const TYPE_CODE_DATETIME  = 'type.datetime';
+    const TYPE_CODE_IMAGE_URL = 'type.image_url';
+
+    public static $types = [
+        self::TYPE_STRING    => self::TYPE_CODE_STRING,
+        self::TYPE_EMAIL     => self::TYPE_CODE_EMAIL,
+        self::TYPE_URL       => self::TYPE_CODE_URL,
+        self::TYPE_PHONE     => self::TYPE_CODE_PHONE,
+        self::TYPE_TEXT      => self::TYPE_CODE_TEXT,
+        self::TYPE_HTML      => self::TYPE_CODE_HTML,
+        self::TYPE_INT       => self::TYPE_CODE_INT,
+        self::TYPE_DOUBLE    => self::TYPE_CODE_DOUBLE,
+        self::TYPE_BOOL      => self::TYPE_CODE_BOOL,
+        self::TYPE_DATE      => self::TYPE_CODE_DATE,
+        self::TYPE_TIME      => self::TYPE_CODE_TIME,
+        self::TYPE_DATETIME  => self::TYPE_CODE_DATETIME,
+        self::TYPE_IMAGE_URL => self::TYPE_CODE_IMAGE_URL,
+    ];
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -38,6 +101,37 @@ class Param
      * @ORM\Column(type="text", nullable=true)
      */
     private $value;
+
+    /**
+     * @ORM\Column(type="smallint")
+     * @Assert\Choice({
+     *     Param::CATEGORY_TEXT_BLOCKS,
+     *     Param::CATEGORY_SITE_SETTINGS,
+     *     Param::CATEGORY_MESSAGES,
+     *     Param::CATEGORY_ORDER_SETTINGS,
+     * })
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="smallint")
+     * @Assert\Choice({
+     *     Param::TYPE_STRING,
+     *     Param::TYPE_EMAIL,
+     *     Param::TYPE_URL,
+     *     Param::TYPE_PHONE,
+     *     Param::TYPE_TEXT,
+     *     Param::TYPE_HTML,
+     *     Param::TYPE_INT,
+     *     Param::TYPE_DOUBLE,
+     *     Param::TYPE_BOOL,
+     *     Param::TYPE_DATE,
+     *     Param::TYPE_TIME,
+     *     Param::TYPE_DATETIME,
+     *     Param::TYPE_IMAGE_URL,
+     * })
+     */
+    private $type;
 
     public function getId()
     {
@@ -76,6 +170,30 @@ class Param
     public function setValue(?string $value): self
     {
         $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function setCategory(int $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
